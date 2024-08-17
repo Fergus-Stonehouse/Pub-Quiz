41bab67e5f12a17454380cde673f7098546de5e9
Fergus Stonehouse <fergus.m.stonehouse@gmail.com>
2024-08-17 15:18:31 +0000

    Add Favicons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 const start = document.getElementById('start');
 start.addEventListener('click', startgame)
 
-
 function startgame(event) {
     console.log('Hello, World');
+    document.getElementsByTagName('game-zone');
+    let swapImg = getElementTagName('img');
+    swapImg.src = "scroll.png";
 }
-console.log(score).value;
-console.log(questionNumber).value;
