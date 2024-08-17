5046cccdd2ed7e4592aebe04d7c55d9650fd6a3e
Fergus Stonehouse <fergus.m.stonehouse@gmail.com>
2024-08-17 15:44:53 +0000

    Swap images on click of image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 const start = document.getElementById('start');
 start.addEventListener('click', startgame)
 
+//---------------------   Set the Game Zone   ------------------------------------------------//
 function startgame(event) {
     console.log('Hello, World');
-    document.getElementsByTagName('game-zone');
-    let swapImg = getElementTagName('img');
-    swapImg.src = "scroll.png";
-}
+    let gameZone = document.getElementById('game-zone');
+    gameZone.innerHTML= `
+                        <div id="question-area">
+                            <img src="assets/images/scroll.png" alt="A scroll">
+                        </div>
+                                
+                        <div id="score-zone">
+                            <p id="score">Score</p>
+                        </div>
+                        `;
+    }
