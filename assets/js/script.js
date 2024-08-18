d9299e03309361a6e665133d31f900de21c5ddad
Fergus Stonehouse <fergus.m.stonehouse@gmail.com>
2024-08-18 11:25:06 +0000

    Add styling to game zone

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,21 +5,32 @@ start.addEventListener('click', startgame)
 function startgame(event) {
     console.log('Hello, World');
     let gameZone = document.getElementById('game-zone');
-    gameZone.innerHTML= `  
-                        <img src="assets/images/scroll.png" class="game-scroll" alt="A scroll">   
-                        <div id="play-area">    
-                            <div id="question-area">
-                                <p>Question</p>
+    gameZone.innerHTML= `   
+                        <section id="play-area">
+                            <div id="quest-num">
+                                <p>Question Number: $(questNum)</p>
                             </div>
+                            <br>
+                            <div id="question-area">
+                                <p>Question:</p>
+                                <p>What is the first letter of the Alphabet?</p>
+                            </div>
+                            <br>
                             <div id="answer-options">
                                 <button id="AnswerA" class="button"><p>A</p></button>
                                 <button id="AnswerB" class="button"><p>B</p></button>
                                 <button id="AnswerC" class="button"><p>C</p></button>
                             </div>
+                            <br>
                             <div id="score-zone">
                                 <p>Score:</p>
                                 <p>score boiler plate</p>
                             </div>
-                        </div>            
+                        </section>            
                         `;
+                        console.log(questNum);
     }
+
+function questNum() {
+    let questNum = getElementById("questNum")
+}
