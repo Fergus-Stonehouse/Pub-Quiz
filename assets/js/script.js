7aa07bd7186467e0d46c7ab846879e1af65ceb40
Fergus Stonehouse <fergus.m.stonehouse@gmail.com>
2024-08-18 20:55:33 +0000

    Building random question generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ start.addEventListener('click', startgame);
 start.addEventListener('click', questNum);
 start.addEventListener('click', newQuestion);
 
+
 //---------------------   Set the Game Zone   ------------------------------------------------//
 function startgame(event) {
     console.log('Hello, World');
@@ -21,9 +22,9 @@ function startgame(event) {
                             </div>
                             
                             <div id="answer-options">
-                                <button id="answerA" class="button"><p>A</p></button>
-                                <button id="answerB" class="button"><p>B - What's it to you, you betentacled thing!</p></button>
-                                <button id="answerC" class="button"><p>C - It's never Lupus.</p></button>
+                                <button id="a" class="button"><p>A</p></button>
+                                <button id="b" class="button"><p>B - What's it to you, you betentacled thing!</p></button>
+                                <button id="c" class="button"><p>C - It's never Lupus.</p></button>
                             </div>
                             
                             <div id="score-zone">
@@ -32,9 +33,6 @@ function startgame(event) {
                             </div>
                         </section>            
                         `;
-
-    console.log(questNum).value;
-    console.log(score).value;
 }
 
 function questNum(event) {
@@ -44,8 +42,40 @@ function questNum(event) {
 
 function newQuestion(event) {
     //-------------------------------------------   randomly generate a new question   ---------------------------------------//
-    let newQuestion = questionSheet[Math.floor(Math.random() * questionSheet.length)];
-    console.log(newQuestion).value
+    let questionSheet = []
+    for (let i = 0; i < 5; i++) {
+    let index = Math.floor(Math.random() * 10)
+        while (questionSheet.includes(index)){
+            index = Math.floor(Math.random() * 10)
+        }
+    questionSheet.push(i)
+}
+// and now to populate the list with actual questions
+
+for (let i = 0; i < questionSheet.length; i++) {
+    chosen_questions[i] = your_questions_list[chosen_questions[i]];
+}
+
+console.log(chosen_questions);
+}
+
+
+
+function checkAnswer(event) {
+    document.getElementById("answer-options").addEventListener('click', function(e){
+        let userAnswer = e.target.id;
+        console.log(userAnswer);
+        return userAnswer;
+      });
+    console.log(userAnswer); console.log(userAnswer).textContent;
+    let correctAnswer = questionSheet[correctAnswer].textContent;
+    if (userAnswer === correctAnswer) {
+        increaseScore(); questNum(); newQuestion ();
+        console.log(correctAnswer);
+    } else {
+        questNum(); newQuestion ();
+        console.log('wrong', questionSheet[correctAnswer])
+    }
 }
 
 function increaseScore() {
@@ -65,66 +95,66 @@ let questionSheet = [
         correctAnswer: "a"
     },
     {
-        question: "What is the first whole number?",
+        question: "Who fired first?",
         answers: {
-            a: "A - 0",
-            b: "B - 1",
-            c: "C - 2"
+            a: "A - Han Solo",
+            b: "B - The Lone Gunman",
+            c: "C - There was a gunshot?"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "How many primary attributes are there in Dungeons and Drangons?",
+        answers: {
+            a: "A - 5",
+            b: "B - 6",
+            c: "C - 8"
         },
         correctAnswer: "b"
     },
     {
-        question: "What is the first letter of the Alphabet?",
+        question: "How many sides on a dodecahedron?",
         answers: {
-            a: "A - ",
-            b: "B - ",
-            c: "C - "
+            a: "A - 6",
+            b: "B - 10",
+            c: "C - 12"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "A man walks in to a bar...",
+        answers: {
+            a: "A - Ouch!",
+            b: "B - ...and has a drink. The End.",
+            c: "C - This isn't the correct answer."
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What is mycology?",
+        answers: {
+            a: "A - the study of mice.",
+            b: "B - the study of microscopes.",
+            c: "C - the study on funghi."
         },
         correctAnswer: "a"
     },
     {
-        question: "What is the first letter of the Alphabet?",
+        question: "What do you call a man with a car on his head?",
         answers: {
-            a: "A - ",
-            b: "B - ",
-            c: "C - "
+            a: "A - Jack",
+            b: "B - not going anywhere.",
+            c: "C - awaiting serious medical attention."
         },
         correctAnswer: "a"
     },
     {
-        question: "What is the first letter of the Alphabet?",
+        question: "Why do I wait for my dad jokes to fully mature?",
         answers: {
-            a: "A - ",
-            b: "B - ",
-            c: "C - "
+            a: "A - The smell... dear Stars, it's for the smell!!!",
+            b: "B - Because then they're fully GROAN...",
+            c: "C - Just... nope..."
         },
-        correctAnswer: "a"
-    },
-    {
-        question: "What is the first letter of the Alphabet?",
-        answers: {
-            a: "A - ",
-            b: "B - ",
-            c: "C - "
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "What is the first letter of the Alphabet?",
-        answers: {
-            a: "A - ",
-            b: "B - ",
-            c: "C - "
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "What is the first letter of the Alphabet?",
-        answers: {
-            a: "A - ",
-            b: "B - ",
-            c: "C - "
-        },
-        correctAnswer: "a"
+        correctAnswer: "b"
     },
 ];
